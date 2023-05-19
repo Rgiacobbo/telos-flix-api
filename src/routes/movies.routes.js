d99c63e7e72551b1f612d0f8b982cfa3ca3b97ad
unknown <pedro28899@gmail.com>
2023-05-19 14:23:11 -0300

    :sparkles: (development of functionality where only admins can create movies)

--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -4,6 +4,7 @@ const moviesController = require("../controllers/movies.controller");
 
 const { verifyAuthenticate } = require("../middlewares/verifyAuthentication");
 const { isAuthenticated } = require("../middlewares/isAuthenticated");
+const {authenticatedUsersRoles,} = require("../middlewares/authenticatedUsersRoles");
 
 const routes = Router();
 
@@ -11,7 +12,7 @@ routes.get("/movies", moviesController.list);
 routes.get("/movies/genres", moviesController.listGenres);
 routes.get("/movies/:id", isAuthenticated, moviesController.getById);
 
-routes.post("/movies", verifyAuthenticate, moviesController.create);
+routes.post("/movies", verifyAuthenticate, authenticatedUsersRoles, moviesController.create);
 
 routes.put("/movies/:id", moviesController.update);
 
