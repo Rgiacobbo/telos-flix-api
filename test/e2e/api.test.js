85866dfddd032d49425c2a754751a03c0386e901
Rgiacobbo <rgiacobbofilho@gmail.com>
2023-07-04 20:10:02 -0300

    :sparkles:

--- test/e2e/api.test.js
+++ test/e2e/api.test.js
@@ -1,5 +1,12 @@
 const { expect, test, describe } = require("@jest/globals");
 
+const request = require("supertest");
+const { Server } = require("../../src/server.js");
+
 describe("Api E2E Test Suite", () => {
-  test.todo("GET / - Deve retornar todos os usuarios e retornar ok");
+  test("GET / - Deve retornar todos os usuarios e retornar ok", async () => {
+    const response = await request(Server).get("/");
+
+    expect(response.status).toBe(200);
+  });
 });
