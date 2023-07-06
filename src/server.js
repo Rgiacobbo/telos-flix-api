f479154fc408a7a2b7298f74fefc5b64af924ed3
Rgiacobbo <rgiacobbofilho@gmail.com>
2023-07-06 12:28:54 -0300

    :sparkles: (testeDeploy) teste Deploy render

--- src/server.js
+++ src/server.js
@@ -17,11 +17,11 @@ const app = express();
 
 app.use(express.json());
 
-// app.use(
-//   cors({
-//     origin: "*",
-//   })
-// );
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
